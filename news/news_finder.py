efed26e49f8ad515b612ef16e685f1add6fb7dc7
string0-0 <skastring00@gmail.com>
2026-05-07 21:55:14 +0900

    sbs 석유 관련 기사입니다

--- news/news_finder.py
+++ news/news_finder.py
@@ -1,37 +1,32 @@
 import feedparser
+import time
 
 # 테스트 url
 # https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=02&plink=RSSREADER
 # https://www.yonhapnewstv.co.kr/category/news/economy/feed/
 url = "https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=02&plink=RSSREADER"
 
-
-# def get_rss_data(url):
-#     feed = feedparser.parse(url)
-#     news_list = feed.entries[:10]
-
-#     result_list = []
-
-#     # title, link, summarty, author, updated는 바로 사용가능 + updated_parsed
-#     for news in news_list:
-#         title = news.title
-#         link = news.link
-#         summary = news.summary
-#         author = news.author
-#         updated_parsed = news.updated_parsed
-#         news_dict = { "title": title, "link": link, "summary": summary, "author": author, "updated_parsed": updated_parsed }
-#         result_list.append(news_dict)
-        
-#     return result_list
-
-def get_oil_news(url):
+def get_oil_news_sbs(url):
     feed = feedparser.parse(url)
-    news_list = feed.entries[:10]
+    news_list = feed.entries[:30]
 
-    with open('./test_new.txt','w', encoding='utf-8-sig') as f :
-        f.write(str(news_list))
-    print(news_list)
+    result_url = []
 
-get_oil_news(url)
+    for news in news_list:
+        title = news.title
+        link = news.link
+        summary = news.summary
+        author = news.author
+        published_parsed = news.published_parsed
+        tags = news.tags
+        news_dict = { "title": title, "link": link, "summary": summary, "author": author, "published_parsed": time.strftime('%Y-%m-%d %H:%M:%S',published_parsed) ,"tags" : list(set(tag['term'] for tag in tags))}
 
-# 'title' 'link' 'published_parsed' 'author' 'summary' 'tags'[n]['term']
+        if '석유' in news_dict["title"] or '석유' in news_dict['tags']:
+            result_url.append(news_dict)
+
+    return result_url
+
+if __name__ == "__main__":
+    print(get_oil_news_sbs(url))
+
+# 'title' 'link' 'published_parsed' 'author' 'summary' 'tags'[n]['term'] 'credit'
